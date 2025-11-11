09c94a6ef8c4576f8212c89de34a9331d47a3de0
tulefat <tolefat1989@gmail.com>
2025-11-11 18:29:52 +0300

    Create model for advertisement, bid, category, user

--- models/catagory.js
+++ models/catagory.js
@@ -1,1 +1,13 @@
-
+const mongoose = require("mongoose")
+const categorySchema = new mongoose.Schema({
+  type: {
+    type: Array,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+const Category = mongoose.model("Category", categorySchema)
+module.exports = Category
